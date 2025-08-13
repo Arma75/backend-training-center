6077d7f8296f1ee690f236ae6533b7b7883780f3
head <head@headui-MacBookAir.local>
2025-08-13 16:13:39 +0900

    Add loggin function using slf4j

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This is a repository for training the backend
 
 Creating a Spring Boot project using Spring Initializr
+Create an API that returns random number
--- src/main/java/com/example/usu1/controller/RandomNumberController.java
+++ src/main/java/com/example/usu1/controller/RandomNumberController.java
@@ -1,17 +1,30 @@
 package com.example.usu1.controller;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @ResponseBody
 public class RandomNumberController {
+    private static final Logger logger = LoggerFactory.getLogger(RandomNumberController.class);
+
     @GetMapping("random")
-    public int getRandomNumber() {
-        return new Random().nextInt(10);
+    public int getRandomNumber(HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        LocalDateTime now = LocalDateTime.now();
+        int returnValue = new Random().nextInt(11);
+        logger.info("[API Call] IP: {}, Time: {}, Returned Value: {}", clientIp, now, returnValue);
+
+        return returnValue;
     }
     
 }
