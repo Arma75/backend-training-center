c5446ce8b872e2f49bf4a17a8ddd2ae2553773d5
head <head@headui-MacBookAir.local>
2025-08-14 21:25:22 +0900

    Create RandomNumberDto and Change random API responses to JSON using RandomNumberDto

--- src/main/java/com/example/usu1/controller/RandomNumberController.java
+++ src/main/java/com/example/usu1/controller/RandomNumberController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.usu1.dto.RandomNumberDto;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +21,16 @@ public class RandomNumberController {
     private static final Logger logger = LoggerFactory.getLogger(RandomNumberController.class);
 
     @GetMapping("random")
-    public int getRandomNumber(HttpServletRequest request, @RequestParam(defaultValue = "0", required = false) int min, @RequestParam(defaultValue = "10", required = false) int max) {
+    public RandomNumberDto getRandomNumber(HttpServletRequest request, @RequestParam(defaultValue = "0", required = false) int min, @RequestParam(defaultValue = "10", required = false) int max) {
         String clientIp = request.getRemoteAddr();
         LocalDateTime now = LocalDateTime.now();
         int returnValue = new Random().nextInt(max - min + 1) + min;
         logger.info("[API Call] IP: {}, Time: {}, Returned Value: {}", clientIp, now, returnValue);
 
-        return returnValue;
+        RandomNumberDto randomNumberDto = new RandomNumberDto();
+        randomNumberDto.setRandomValue(returnValue);
+        
+        return randomNumberDto;
     }
     
 }
