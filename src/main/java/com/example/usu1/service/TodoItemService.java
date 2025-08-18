05ab45f8adfb079dfa4a1cf11b033243e3d04f8b
head <head@headui-MacBookAir.local>
2025-08-18 20:58:40 +0900

    Delete TodoItemDao and apply ResponseEntity

--- src/main/java/com/example/usu1/service/TodoItemService.java
+++ src/main/java/com/example/usu1/service/TodoItemService.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.example.usu1.dao.TodoItemDao;
 import com.example.usu1.dto.TodoItemDto;
+import com.example.usu1.mapper.TodoItemMapper;
 
 @Service
 public class TodoItemService {
     @Autowired
-    private TodoItemDao todoItemDao;
+    private TodoItemMapper todoItemMapper;
 
     public String addTodoItem(@RequestBody TodoItemDto todoItemDto) {
-        return todoItemDao.addTodoItem(todoItemDto);
+        todoItemMapper.insertTodoItem(todoItemDto);
+        return "OK";
     }
 }
