0d8443f91c63a6843d05e502f2a0d47a46b04c6d
head <head@headui-MacBookAir.local>
2025-08-19 13:52:25 +0900

    Apply postgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS todo_item (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content VARCHAR(1000),
     is_completed BOOLEAN,
